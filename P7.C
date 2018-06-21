69c9b74af84574b4188274681baaef98c9523c41
joaovf22 <38232245+joaovf22@users.noreply.github.com>
2018-06-20 21:51:01 -0300

    Add files via upload

---
+++ P7.C
@@ -0,0 +1,20 @@
+// João Victor Oliveira Ferreira	
+// 11721EAU006
+
+#include <stdio.h>
+
+int ackerman(int, int);
+int main()
+{	int m, n;
+	printf("Digite os valores para m e n, respectivamente: ");
+	scanf("%d %d", &m, &n);
+
+	printf("Saida = %d ", ackerman(m, n));
+	return 0;
+}
+int ackerman(int m, int n)
+{	if(m==0){	return n + 1;	}
+	else if(m > 0 && n==0) {	ackerman(m-1, 1);	}
+	else if(m > 0 && n > 0) {	ackerman(m - 1, ackerman(m, n - 1));
+	}
+}
